18138a34a441a094bd4538a25036fb59d380a119
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-02-09 13:22:19 -0300

    Finalizada aula 30

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const [user, setUser] = useState({})
+
+  function ChamarRegistro(e){
+    e.preventDefault();
+    
+    alert("Usuario registrado com Sucesso!!")
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando Usuario</h1>
+
+      <form onSubmit={ChamarRegistro}>
+        <label>Nome:</label><br/>
+        <input 
+          placeholder="Digite seu Nome"
+          value={nome}
+          onChange={(e)=> setNome(e.target.value)}
+          /><br/>
+
+        <label>Email:</label><br/>
+        <input 
+        placeholder="Digite seu Email"
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
+        /><br/>
+
+        <label>Idade:</label><br/>
+        <input 
+          placeholder="Digite sua Idade"
+          value={idade}
+          onChange={(e)=> setIdade(e.target.value)}
+        /><br/><br/>
+
+        <button type="submit">Registrar</button>
+
+        
+      </form>
+      <br/>
+      <br/>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span><br/>
+        <span>Idade: {user.idade}</span><br/>
+        <span>Email: {user.email}</span><br/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
